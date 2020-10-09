b4183473dac75309b3f136488af81272d959cd57
Diedrich Swetlik <daswetlik19@coe.edu>
2020-10-09 00:58:03 -0500

    Added two functions, GetCourseCredits() and GetGradePointAverage() for use in the front-end.

--- CS390/Student.cs
+++ CS390/Student.cs
@@ -58,6 +58,70 @@ namespace CS390
         {
             return enrolledCourses;
         }
+
+        public float GetCourseCredits()
+        {
+            float x = 0.0f;
+            foreach(Course course in courseHistory)
+            {
+                x += Convert.ToSingle(course.GetCourseCredit());
+            }
+            return x;
+        }
+
+        public float GetGradePointAverage()
+        {
+            float x = 0.0f;
+            int courseCount = 0;
+            foreach(Course course in courseHistory)
+            {
+                switch(course.GetGrade())
+                {
+                    case "A":
+                        x += 4f;
+                        courseCount++;
+                        break;
+                    case "A-":
+                        x += 3.7f;
+                        courseCount++;
+                        break;
+                    case "B+":
+                        x += 3.3f; courseCount++;
+                        break;
+                    case "B":
+                        x += 3f; courseCount++;
+                        break;
+                    case "B-":
+                        x += 2.7f; courseCount++;
+                        break;
+                    case "C+":
+                        x += 2.3f; courseCount++;
+                        break;
+                    case "C":
+                        x += 2f; courseCount++;
+                        break;
+                    case "C-":
+                        x += 1.7f; courseCount++;
+                        break;
+                    case "D+":
+                        x += 1.3f; courseCount++;
+                        break;
+                    case "D":
+                        x += 1f; courseCount++;
+                        break;
+                    case "D-":
+                        x += 0.7f; courseCount++;
+                        break;
+                    case "F":
+                        x += 0; courseCount++;
+                        break;
+                    case "WF":
+                        x += 0; courseCount++;
+                        break;
+                }
+            }
+            return (x / courseCount);
+        }
     }
 }
 
