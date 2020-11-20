b7d3fd867b958b6d220839db0aacd61fc1c42b52
itscoe <itslagle@coe.edu>
2020-11-20 10:29:43 -0600

    Added all managerial controls

--- CS390/AdminManagerDashboard.Designer.cs
+++ CS390/AdminManagerDashboard.Designer.cs
@@ -67,9 +67,31 @@
             this.label7 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label8 = new System.Windows.Forms.Label();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox5 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox4 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox3 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox6 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox2 = new System.Windows.Forms.CheckedListBox();
+            this.comboBox7 = new System.Windows.Forms.ComboBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.comboBox8 = new System.Windows.Forms.ComboBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.button15 = new System.Windows.Forms.Button();
             this.Remove = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.ChangeProfessor = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            this.button13 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -175,7 +197,7 @@
             this.dataGridView1.Location = new System.Drawing.Point(109, 24);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 50;
-            this.dataGridView1.Size = new System.Drawing.Size(939, 402);
+            this.dataGridView1.Size = new System.Drawing.Size(939, 294);
             this.dataGridView1.TabIndex = 1;
             // 
             // label1
@@ -279,7 +301,7 @@
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.RemoveFaculty});
-            this.dataGridView3.Location = new System.Drawing.Point(109, 25);
+            this.dataGridView3.Location = new System.Drawing.Point(109, 24);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 50;
             this.dataGridView3.Size = new System.Drawing.Size(939, 400);
@@ -463,16 +485,6 @@
             this.label8.Text = "Status";
             this.label8.Visible = false;
             // 
-            // Remove
-            // 
-            this.Remove.HeaderText = "Remove";
-            this.Remove.Name = "Remove";
-            // 
-            // ChangeProfessor
-            // 
-            this.ChangeProfessor.HeaderText = "Professor";
-            this.ChangeProfessor.Name = "ChangeProfessor";
-            // 
             // button13
             // 
             this.button13.Location = new System.Drawing.Point(807, 432);
@@ -483,11 +495,314 @@
             this.button13.UseVisualStyleBackColor = true;
             this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(809, 354);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(87, 23);
+            this.button14.TabIndex = 41;
+            this.button14.Text = "Change Time";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM"});
+            this.comboBox5.Location = new System.Drawing.Point(696, 356);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(82, 21);
+            this.comboBox5.TabIndex = 40;
+            // 
+            // checkedListBox5
+            // 
+            this.checkedListBox5.FormattingEnabled = true;
+            this.checkedListBox5.Items.AddRange(new object[] {
+            "M",
+            "T",
+            "W",
+            "R",
+            "F"});
+            this.checkedListBox5.Location = new System.Drawing.Point(651, 324);
+            this.checkedListBox5.Name = "checkedListBox5";
+            this.checkedListBox5.Size = new System.Drawing.Size(38, 79);
+            this.checkedListBox5.TabIndex = 39;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM"});
+            this.comboBox4.Location = new System.Drawing.Point(563, 356);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(82, 21);
+            this.comboBox4.TabIndex = 38;
+            // 
+            // checkedListBox4
+            // 
+            this.checkedListBox4.FormattingEnabled = true;
+            this.checkedListBox4.Items.AddRange(new object[] {
+            "M",
+            "T",
+            "W",
+            "R",
+            "F"});
+            this.checkedListBox4.Location = new System.Drawing.Point(518, 324);
+            this.checkedListBox4.Name = "checkedListBox4";
+            this.checkedListBox4.Size = new System.Drawing.Size(38, 79);
+            this.checkedListBox4.TabIndex = 37;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM"});
+            this.comboBox3.Location = new System.Drawing.Point(430, 356);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(82, 21);
+            this.comboBox3.TabIndex = 36;
+            // 
+            // checkedListBox3
+            // 
+            this.checkedListBox3.FormattingEnabled = true;
+            this.checkedListBox3.Items.AddRange(new object[] {
+            "M",
+            "T",
+            "W",
+            "R",
+            "F"});
+            this.checkedListBox3.Location = new System.Drawing.Point(385, 324);
+            this.checkedListBox3.Name = "checkedListBox3";
+            this.checkedListBox3.Size = new System.Drawing.Size(38, 79);
+            this.checkedListBox3.TabIndex = 35;
+            // 
+            // comboBox6
+            // 
+            this.comboBox6.FormattingEnabled = true;
+            this.comboBox6.Items.AddRange(new object[] {
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM"});
+            this.comboBox6.Location = new System.Drawing.Point(297, 356);
+            this.comboBox6.Name = "comboBox6";
+            this.comboBox6.Size = new System.Drawing.Size(82, 21);
+            this.comboBox6.TabIndex = 34;
+            // 
+            // checkedListBox2
+            // 
+            this.checkedListBox2.FormattingEnabled = true;
+            this.checkedListBox2.Items.AddRange(new object[] {
+            "M",
+            "T",
+            "W",
+            "R",
+            "F"});
+            this.checkedListBox2.Location = new System.Drawing.Point(252, 324);
+            this.checkedListBox2.Name = "checkedListBox2";
+            this.checkedListBox2.Size = new System.Drawing.Size(38, 79);
+            this.checkedListBox2.TabIndex = 33;
+            // 
+            // comboBox7
+            // 
+            this.comboBox7.FormattingEnabled = true;
+            this.comboBox7.Items.AddRange(new object[] {
+            "7:00 AM",
+            "8:00 AM",
+            "9:00 AM",
+            "10:00 AM",
+            "11:00 AM",
+            "12:00 PM",
+            "1:00 PM",
+            "2:00 PM",
+            "3:00 PM",
+            "4:00 PM"});
+            this.comboBox7.Location = new System.Drawing.Point(164, 356);
+            this.comboBox7.Name = "comboBox7";
+            this.comboBox7.Size = new System.Drawing.Size(82, 21);
+            this.comboBox7.TabIndex = 32;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "M",
+            "T",
+            "W",
+            "R",
+            "F"});
+            this.checkedListBox1.Location = new System.Drawing.Point(119, 324);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(38, 79);
+            this.checkedListBox1.TabIndex = 31;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Location = new System.Drawing.Point(432, 412);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(34, 13);
+            this.label9.TabIndex = 49;
+            this.label9.Text = "Seats";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Location = new System.Drawing.Point(326, 412);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 13);
+            this.label10.TabIndex = 48;
+            this.label10.Text = "Credits";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Location = new System.Drawing.Point(220, 412);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(71, 13);
+            this.label11.TabIndex = 47;
+            this.label11.Text = "Course Name";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.Location = new System.Drawing.Point(117, 414);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(54, 13);
+            this.label12.TabIndex = 46;
+            this.label12.Text = "Course ID";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(435, 430);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 45;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(329, 430);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 44;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(223, 430);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 43;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(117, 430);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 42;
+            // 
+            // comboBox8
+            // 
+            this.comboBox8.FormattingEnabled = true;
+            this.comboBox8.Location = new System.Drawing.Point(541, 430);
+            this.comboBox8.Name = "comboBox8";
+            this.comboBox8.Size = new System.Drawing.Size(121, 21);
+            this.comboBox8.TabIndex = 50;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.Location = new System.Drawing.Point(538, 414);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(41, 13);
+            this.label13.TabIndex = 51;
+            this.label13.Text = "Faculty";
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(668, 430);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(75, 23);
+            this.button15.TabIndex = 52;
+            this.button15.Text = "Add Course";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.HeaderText = "Select";
+            this.Remove.Name = "Remove";
+            // 
+            // ChangeProfessor
+            // 
+            this.ChangeProfessor.HeaderText = "Professor";
+            this.ChangeProfessor.Name = "ChangeProfessor";
+            // 
             // AdminManagerDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1049, 457);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.comboBox8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.checkedListBox5);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.checkedListBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.checkedListBox3);
+            this.Controls.Add(this.comboBox6);
+            this.Controls.Add(this.checkedListBox2);
+            this.Controls.Add(this.comboBox7);
+            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.button13);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.textBox6);
@@ -572,8 +887,30 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.CheckedListBox checkedListBox5;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.CheckedListBox checkedListBox4;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.CheckedListBox checkedListBox3;
+        private System.Windows.Forms.ComboBox comboBox6;
+        private System.Windows.Forms.CheckedListBox checkedListBox2;
+        private System.Windows.Forms.ComboBox comboBox7;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.ComboBox comboBox8;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Button button15;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Remove;
         private System.Windows.Forms.DataGridViewComboBoxColumn ChangeProfessor;
-        private System.Windows.Forms.Button button13;
     }
 }
